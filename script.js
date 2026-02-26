e6fa8ec0bcfa0d3666c33c67e7d826e71f46d127
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2026-02-26 14:01:59 -0300

    feat: style da imagem e baseadas na idade

--- script.js
+++ script.js
@@ -6,16 +6,45 @@ function verificar() {
     var idade = ano - Number(nascimento.value)
     var fsex = document.getElementsByName('radsex')
     var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    img.style.width = '150px'
+    img.style.height = '150px'
+    img.style.display = 'block'
+    img.style.margin = 'auto' 
+    img.style.borderRadius = '50%'
 
     if (ano < Number(nascimento.value)) {
         window.alert('[ERRO] Verifique os dados inseridos!')
     } else {
         if (fsex[0].checked) {
             genero = 'homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-crianca-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            }
+            
         } else if (fsex[1].checked) {
             genero = 'mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-crianca-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'foto-adulto-m.png')
+            }
         }
 
+        msg.style.textAlign = 'center'
+        msg.appendChild(img)
         msg.innerHTML = `Detectamos <strong>${genero}</strong> com ${idade} anos!`
+        
     }
 }
