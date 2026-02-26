5507b6a835c770618701b833220b6c01bfddee29
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2026-02-26 14:09:40 -0300

    fix: nomes das imagens

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function verificar() {
     img.style.width = '150px'
     img.style.height = '150px'
     img.style.display = 'block'
-    img.style.margin = 'auto' 
+    img.style.margin = 'auto'
     img.style.borderRadius = '50%'
 
     if (ano < Number(nascimento.value)) {
@@ -20,31 +20,31 @@ function verificar() {
         if (fsex[0].checked) {
             genero = 'homem'
             if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'foto-crianca-f.png')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'foto-jovem-f.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'foto-adulto-f.png')
-            } else {
-                img.setAttribute('src', 'foto-adulto-f.png')
-            }
-            
-        } else if (fsex[1].checked) {
-            genero = 'mulher'
-            if (idade >= 0 && idade < 10) {
                 img.setAttribute('src', 'foto-crianca-m.png')
             } else if (idade < 21) {
                 img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50) {
                 img.setAttribute('src', 'foto-adulto-m.png')
             } else {
-                img.setAttribute('src', 'foto-adulto-m.png')
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+            
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-crianca-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-f.png')
             }
         }
 
         msg.style.textAlign = 'center'
-        msg.appendChild(img)
         msg.innerHTML = `Detectamos <strong>${genero}</strong> com ${idade} anos!`
+        msg.appendChild(img)
         
     }
 }
